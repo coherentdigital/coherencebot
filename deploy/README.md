4d73554a1837658846b0ae0136801bd53514e27c
PeterCiuffetti <pciuffetti@cohenerntdigital.net>
2021-05-03 22:24:40 -0400

    Adding java; fixing ant

--- deploy/README.md
+++ deploy/README.md
@@ -1,12 +1,17 @@
 # Deploying CoherenceBot
 
 ## Make an Archive
+From the repo root:
+
  git archive -o coherencebot.tar.gz HEAD
 
 ## Copy to S3
 
   aws s3 cp coherencebot.tar.gz s3://coherencebot/deploy/
   aws s3 cp --recursive conf s3://coherencebot/deploy/conf/
-  aws s3 cp bootstrap.sh s3://coherencebot/deploy/bootstrap/bootstrap.sh
+  aws s3 cp deploy/bootstrap.sh s3://coherencebot/deploy/bootstrap/bootstrap.sh
 
 ## Run AWS CLI to Create
+Pass in the region to create the cluster in.
+
+  ./deploy/create-cluster.sh us-east-2
--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -1,10 +1,19 @@
 # The command-line arg $1 contains a region that this CoherenceBot installation is responsible for.
-grep -v COHERENCEBOT ~/.bashrc | grep -v IVY_CACHE_ROOT > ~/.bashrc_new
+grep -v COHERENCEBOT ~/.bashrc | grep -v IVY_CACHE_ROOT | grep -v JAVA_HOME > ~/.bashrc_new
 echo "# COHERENCEBOT_REGION that this cluster is responsible for:" >> ~/.bashrc_new
 echo "export COHERENCEBOT_REGION=$1" >> ~/.bashrc_new
 echo "export IVY_CACHE_ROOT=/mnt/coherencebot/ivy-cache" >> ~/.bashrc_new
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64" >> ~/.bashrc_new
 mv ~/.bashrc_new ~/.bashrc
 source ~/.bashrc
+
+# Install JAVA SDK
+sudo amazon-linux-extras enable corretto8
+sudo yum clean metadata
+sudo yum install -y java-1.8.0-amazon-corretto-devel
+
+# Install ANT
+sudo yum install -y ant
 
 # Copy down the coherencebot repo export from S3. Untar it, and build it.
 cd /mnt/
@@ -14,4 +23,9 @@ cd coherencebot
 aws s3 cp s3://coherencebot/deploy/coherencebot.tar.gz .
 tar -xvzf coherencebot.tar.gz
 aws s3 cp --recursive s3://coherencebot/deploy/conf/ conf
-ant clean runtime
+if command -v ant ; then
+    echo "Ant available" > /tmp/install-results.txt
+	ant clean runtime
+else
+    echo "Ant unavailable" > /tmp/install-results.txt
+fi
