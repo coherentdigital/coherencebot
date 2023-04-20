2729876302816838607f5d3c666f5574ead6fdd0
PeterCiuffetti <pciuffetti@cohenerntdigital.net>
2023-04-20 08:20:17 -0400

    Adding stats for allowed hosts in CB Dashboard

--- src/java/org/apache/nutch/util/FormatCrawlStats.java
+++ src/java/org/apache/nutch/util/FormatCrawlStats.java
@@ -8,6 +8,7 @@ import java.net.URISyntaxException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +30,7 @@ public class FormatCrawlStats extends Configured implements Tool {
       .getLogger(MethodHandles.lookup().lookupClass());
 
   Map<String,JSONObject> hostsMeta = new HashMap<String,JSONObject>();
+  Map<String,JSONObject> allowedHostsMeta = new HashMap<String,JSONObject>();
 
   public int createCrawlComplete() {
     // Read in seeds.txt which has all the collections this bot is working on.
@@ -53,6 +55,7 @@ public class FormatCrawlStats extends Configured implements Tool {
           JSONObject hostObj = hostsMeta.get(host);
           if (hostObj == null) {
             hostsMeta.put(host, collection);
+            hostObj = collection;
           } else {
             // Merge HOST objects
             JSONArray hostTitles = hostObj.getJSONArray("collection_titles");
@@ -85,7 +88,37 @@ public class FormatCrawlStats extends Configured implements Tool {
             } else {
               LOG.error("No existing collection seeds for {}", host);
             }
+            JSONArray hostDomains = hostObj.getJSONArray("org_domains");
+            JSONArray orgDomains = collection.getJSONArray("org_domains");
+            if (orgDomains != null && hostDomains != null) {
+              for (int i = 0; i< orgDomains.length(); i++) {
+                String domain = (String)orgDomains.get(i);
+                hostDomains.put(domain);
+              }
+            } else {
+              LOG.error("No existing collection domains for {}", host);
+            }
             hostsMeta.put(host, hostObj);
+          }
+          // Add a mapping for all unique allowed hosts
+          JSONArray orgDomains = hostObj.getJSONArray("org_domains");
+          if (orgDomains != null) {
+            for (int i = 0; i < orgDomains.length(); i++) {
+              String allowedHost = (String)orgDomains.get(i);
+              if (!allowedHost.equals(host)) {
+                if (allowedHostsMeta.get(allowedHost) == null) {
+                  JSONObject hostMeta = new JSONObject();
+                  hostMeta.put("host", allowedHost);
+                  hostMeta.put("id", allowedHost);
+                  hostMeta.put("collection_ids", hostObj.get("collection_ids"));
+                  hostMeta.put("collection_titles", hostObj.get("collection_titles"));
+                  hostMeta.put("collection_seeds", hostObj.get("collection_seeds"));
+                  hostMeta.put("org_slug", hostObj.get("org_slug"));
+                  hostMeta.put("org_domains", hostObj.get("org_domains"));
+                  allowedHostsMeta.put(allowedHost, hostMeta);
+                }
+              }
+            }
           }
         }
       }
@@ -107,15 +140,26 @@ public class FormatCrawlStats extends Configured implements Tool {
           if (hostMeta == null) {
             LOG.warn("No collection info in seeds.txt for host {}", host);
             // The nutch database is reporting data for a host not in the seed list.
-            // Make in an dashboard record with unknown values for the collection meta.
+            // These are typically allowed_hosts other than the seed.
+            // Make a dashboard record with unknown or mapped values for the collection meta.
             hostMeta = new JSONObject();
             hostMeta.put("host", host);
             hostMeta.put("id", host);
-            hostMeta.put("collection_ids", unknown);
-            hostMeta.put("collection_titles", unknown);
-            hostMeta.put("collection_seeds", unknown);
-            hostMeta.put("org_slug", "unknown");
             hostMeta.put("type", "host");
+            JSONObject allowedHostMeta = allowedHostsMeta.get(host);
+            if (allowedHostMeta != null) {
+              hostMeta.put("collection_ids", allowedHostMeta.get("collection_ids"));
+              hostMeta.put("collection_titles", allowedHostMeta.get("collection_titles"));
+              hostMeta.put("collection_seeds", allowedHostMeta.get("collection_seeds"));
+              hostMeta.put("org_slug", allowedHostMeta.get("org_slug"));
+              hostMeta.put("org_domains", allowedHostMeta.get("org_domains"));
+            } else {
+              hostMeta.put("collection_ids", unknown);
+              hostMeta.put("collection_titles", unknown);
+              hostMeta.put("collection_seeds", unknown);
+              hostMeta.put("org_slug", "unknown");
+              hostMeta.put("org_domains", unknown);
+            }
           }
           if (cc.has("unfetched")) {
             hostMeta.put("unfetched", cc.get("unfetched"));
@@ -149,7 +193,6 @@ public class FormatCrawlStats extends Configured implements Tool {
       hostsMetaJson.put(hostObj);
     }
 
-
     try {
       LOG.info("Writing hosts.json");
       FileWriter jsonWriter = new FileWriter("hosts.json");
@@ -177,8 +220,16 @@ public class FormatCrawlStats extends Configured implements Tool {
           jo.put("id", uri.getHost());
           jo.put("collection_titles", new JSONArray("[\"" + columns[1].split("=")[1] + "\"]"));
           jo.put("collection_ids", new JSONArray("[\"" + columns[2].split("=")[1] + "\"]"));
-          jo.put("collection_seeds", new JSONArray("[\"" + columns[3].split("=")[1] + "\"]"));
+          jo.put("collection_seeds", new JSONArray("[\"" + columns[3].split("=", 2)[1] + "\"]"));
           jo.put("org_slug", columns[4].split("=")[1]);
+          if (columns.length > 5) {
+            String domainStr = columns[5].split("=")[1];
+            List<String> domains = Arrays.asList(domainStr.split(";"));
+            JSONArray jaDomains = new JSONArray(domains);
+            jo.put("org_domains", jaDomains);
+          } else {
+            jo.put("org_domains", new JSONArray("[\"" + uri.getHost() + "\"]"));
+          }
           jo.put("type", "host");
           String region = System.getenv("COHERENCEBOT_REGION");
           if (region == null) {
